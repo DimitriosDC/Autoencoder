e0764afe91f8debe5285b48ae8b2a33efcc97c83
Laura R Edmondson <32597905+lauraredmondson@users.noreply.github.com>
2023-11-16 11:22:06 +0000

    Add title to grid plots function

--- plotting_functions.py
+++ plotting_functions.py
@@ -65,35 +65,35 @@ def plot_reconstructed_input(model, data_samples, num, receptors_each_region, sh
     print(reconstructed.shape)
 
 
-def plot_grids(data, x_figures=5, y_figures=10, receptor_split=900, region_shape=30):
-    
+def plot_grids(data, x_figures=5, y_figures=10, receptor_split=900, region_shape=30, title=''):
+
     # flip data if short axis not first
     #if np.size(data,0) > np.size(data,1):
     #    data = data.T
-    
+
     fig = plt.figure(figsize=(y_figures,x_figures)) # create outer figure
-    subfigs = fig.subfigures(x_figures, y_figures) # create figure subpanels
-    
+    subfigs2 = fig.subfigures(2,1, height_ratios=[1,13])
+    subfigs = subfigs2[1].subfigures(x_figures, y_figures) # create figure subpanels
+
+    subfigs2[0].suptitle(title, fontsize=15)
+
     for outer_idx, subfig in enumerate(subfigs.flat): # for each suplot create inner figures
 
         if outer_idx < np.size(data,0): # check that there is enough data for the plot
             subfig.suptitle(f'{outer_idx+1}') # add index of plot
 
             axs = subfig.subplots(1, 2) # create inner subplot with two panels
-            
+
             ax_data = data[outer_idx,:] # get data
             ax_data_r1 = ax_data[:receptor_split].reshape(region_shape,region_shape) # reshape region 1 data
             ax_data_r2 = ax_data[receptor_split:].reshape(region_shape,region_shape) # reshape region 2 data
-            
+
             both_reshape = [ax_data_r1,ax_data_r2] # create list of data
-            
+
             min_data = np.min(ax_data) # calculate min of data
             max_data = np.max(ax_data) # calculate max of data
-            
+
             for inner_idx, ax in enumerate(axs.flat): # plot for each region
                 ax.imshow(both_reshape[inner_idx], vmin=min_data, vmax=max_data) # create plot
                 ax.set_xticks([]) # remove ticks
                 ax.set_yticks([]) # remove ticks
-        
-            subfig.subplots_adjust(top=0.9) # shift plot
-    
